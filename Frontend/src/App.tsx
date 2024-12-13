4474e4148376884ad3faf71565554fd3c15e23ca
DominikSzczepaniak <szczepaniakdominik5@gmail.com>
2024-12-13 21:26:29 +0100

    1. designy 2. shadcn theme

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button } from './components/ui/button'
+import { ThemeProvider } from "@/components/theme-provider"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <p className='text-red-300'>asdfasdfasdf</p>
-      <Button>Chuj</Button>
-    </>
+      <Button>musisz tego sluchac glosno</Button>
+    </ThemeProvider>
   )
 }
 
