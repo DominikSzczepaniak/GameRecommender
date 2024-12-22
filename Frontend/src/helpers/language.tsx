55b7c66948b01db389f96ae1524ab1df87894813
slxshxr17 <szczepaniakdominik5@gmail.com>
2024-12-22 16:28:31 +0100

    changed eslint options

--- Frontend/src/helpers/language.tsx
+++ Frontend/src/helpers/language.tsx
@@ -5,8 +5,10 @@ function getLanguage() {
 export function getLanguageFile(){
     switch(getLanguage()){
         case "pl":
+            // eslint-disable-next-line @typescript-eslint/no-require-imports
             return require("../translations/pl.json");
         default:
+            // eslint-disable-next-line @typescript-eslint/no-require-imports
             return require("../translations/en.json");
     }
 } 
