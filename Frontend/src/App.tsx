20dbe086c0ad8cb66c55ccca7c5a8d0de7777f31
DominikSzczepaniak <62748899+DominikSzczepaniak@users.noreply.github.com>
2024-12-22 17:41:00 +0100

    Linters setup (#18)
    * husky prettier and eslint setup
    
    * lintered some file and added some settings
    
    * Final changes to prettier options, formatted all files

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from './components/ui/button'
-import { ThemeProvider } from "@/components/theme-provider"
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Button } from './components/ui/button';
+import { ThemeProvider } from '@/components/theme-provider';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <p className='text-red-300'>asdfasdfasdf</p>
+      <p className="text-red-300">asdfasdfasdf</p>
       <Button>musisz tego sluchac glosno</Button>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
