9196555aa15f0c0fa20956f21dcd66fb091ca09a
DominikSzczepaniak <62748899+DominikSzczepaniak@users.noreply.github.com>
2024-12-22 20:53:57 +0100

    Linters setup (#19)
    * husky prettier and eslint setup
    
    * lintered some file and added some settings
    
    * Final changes to prettier options, formatted all files
    
    * added dprint instead of prettier
    
    * formatted all

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
 	{ ignores: ['dist'] },
@@ -11,7 +10,6 @@ export default tseslint.config(
 		extends: [
 			js.configs.recommended,
 			...tseslint.configs.recommended,
-			eslintConfigPrettier,
 		],
 		files: ['src/**/*.{ts,tsx}'],
 		ignores: ['src/components/ui/**.*'],
--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import { Button } from './components/ui/button';
-import { ThemeProvider } from '@/components/theme-provider';
-import './App.css';
+import viteLogo from '/vite.svg'
+import { ThemeProvider } from '@/components/theme-provider'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Button } from './components/ui/button'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <p className="text-red-300">asdfasdfasdf</p>
-      <Button>musisz tego sluchac glosno</Button>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <p className='text-red-300'>
+        asdfasdfasdf
+      </p>
+      <Button>
+        musisz tego sluchac glosno
+      </Button>
     </ThemeProvider>
   );
 }
