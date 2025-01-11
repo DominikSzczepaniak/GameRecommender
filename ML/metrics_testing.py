f7357fe34d39a1aeb93c0cb962739f5131692eeb
slxshxr17 <szczepaniakdominik5@gmail.com>
2025-01-11 21:46:00 +0100

    ML metrics - diversity, novelity, coverage popularity, MAP, Average recall

---
+++ ML/metrics_testing.py
@@ -0,0 +1,5 @@
+from recmetrics import novelty, prediction_coverage, catalog_coverage, _ark, _apk, mark, mapk, personalization
+
+#diversity = catalog_coverage 
+#novelty = novelty
+#coverage popularity = prediction coverage
