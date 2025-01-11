a04a7e005d4c77fc9d4e06d28e9d3847758cff62
slxshxr17 <szczepaniakdominik5@gmail.com>
2025-01-11 21:49:31 +0100

    hitrate

--- ML/metrics_testing.py
+++ ML/metrics_testing.py
@@ -3,3 +3,16 @@ from recmetrics import novelty, prediction_coverage, catalog_coverage, _ark, _ap
 #diversity = catalog_coverage 
 #novelty = novelty
 #coverage popularity = prediction coverage
+def hitrate(predicted: List[List], actual: List[List], k):
+    def hitrate_for_user(predicted, actual, k):
+        if len(predicted) > k:
+            predicted = predicted[:k]
+        for item in predicted:
+            if item in actual:
+                return 1
+        return 0
+    
+    hits = 0 
+    for i in range(len(predicted)):
+        hits += hitrate_for_user(predicted[i], actual[i], k)
+    return hits / len(predicted)
